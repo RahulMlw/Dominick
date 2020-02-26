a68ae02e391a63106b9645a262d7bf4e68c49f73
Rahul Bhandari <rahulbhandari@rahuls-mac.local>
2020-02-26 22:49:04 +0530

    Initial Commit

---
+++ dominick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dominick
+//
+//  Created by Rahul Bhandari on 26/02/20.
+//  Copyright © 2020 Rahul Bhandari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
